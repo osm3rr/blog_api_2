c4a35834ad7cebc21b780aa878c8f104a4b66077
osm3rr <parabavireo@gmail.com>
2024-04-01 11:55:32 -0400

    serializers, views, urls added

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,7 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # local
     'accounts.apps.AccountsConfig',
+    'posts.apps.PostsConfig',
+    # terceros
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
