5bf69b60f8541c047f0e00c033a767807d0b68fd
osm3rr <parabavireo@gmail.com>
2024-04-03 10:45:43 -0400

    permissions added

--- accounts/admin.py
+++ accounts/admin.py
@@ -10,6 +10,7 @@ class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
     model = CustomUser
     list_display = [
+        "id",
         "email",
         'username',
         "nickname",
